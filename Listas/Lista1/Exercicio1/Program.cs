410f5251c1e8697f2fb9f8c33440ac66c590e484
CamilaPedrozaCosta <camilacostavdc@gmail.com>
2024-02-20 19:40:26 -0300

    TESTE

---
+++ Listas/Lista1/Exercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+     static void Main(string[] args)
+     {
+    
+        
+        Console.WriteLine("Olá! Digite seu nome: ");
+        string nome = Console.ReadLine();
+        
+
+        double nota1 = double.Parse(Console.ReadLine());
+        
+
+
+     }
+
+}
+
+
