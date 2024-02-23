17c6ec79a2609dcceb1cc4a8dd389950e16e2a07
CamilaPedrozaCosta <camilacostavdc@gmail.com>
2024-02-23 17:35:58 -0300

    PUSH

--- Listas/Lista1/Exercicio1/obj/Debug/net8.0/Exercicio1.AssemblyInfo.cs
+++ Listas/Lista1/Exercicio1/obj/Debug/net8.0/Exercicio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+410f5251c1e8697f2fb9f8c33440ac66c590e484")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a676f06b3b1ab62199bb462c4e36dc111b21ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
