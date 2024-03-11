498b9281a883215d66f9188c48f494add206b77c
Camila Pedroza <94586261+CamilaPedroza@users.noreply.github.com>
2024-03-11 16:39:05 -0300

    Update Program.cs

--- Listas/Lista1/Exercicio2/Program.cs
+++ Listas/Lista1/Exercicio2/Program.cs
@@ -2,20 +2,17 @@
 
 class Program
 {
-  
   public static void Main (string[] args)
-  {
-  
-  Console.WriteLine("Digite a base do Triangulo: ");
-  double baseTriangulo = double.Parse(Console.ReadLine());
+    {
+    Console.WriteLine("Digite a base do Triangulo: ");
+    double baseTriangulo = double.Parse(Console.ReadLine());
 
-  Console.WriteLine("Digite a altura do Triangulo: ");
-  double alturaTriangulo = double.Parse(Console.ReadLine());
+    Console.WriteLine("Digite a altura do Triangulo: ");
+    double alturaTriangulo = double.Parse(Console.ReadLine());
 
-  double areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
-  bool ehMaiorQ20 = areaTriangulo > 20;
+    double areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+    bool ehMaiorQ20 = areaTriangulo > 20;
 
-  Console.WriteLine("A área do Triangulo é maior que 20? " + ehMaiorQ20 + "! A área do Triangulo é: " + areaTriangulo + ".");
-
-  }
+    Console.WriteLine("A área do Triangulo é maior que 20? " + ehMaiorQ20 + "! A área do Triangulo é: " + areaTriangulo + ".");
+   }
 }
